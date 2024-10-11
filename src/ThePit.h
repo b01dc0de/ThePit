9a8195e61acc6b0abb23a48c8749e82a2d692c02
CKA <163610791+b01dc0de@users.noreply.github.com>
2024-10-10 20:02:15 -0500

    Init commit for ThePit

---
+++ src/ThePit.h
@@ -0,0 +1,14 @@
+﻿#ifndef THE_PIT_H
+#define THE_PIT_H
+
+// CKA_TODO: Implement different printf
+#define DBG_LOGF(...) printf(__VA_ARGS__)
+#define DBG_BREAKPOINT() DebugBreak()
+#define THE_PIT_ASSERT(Expr) \
+	if (!(Expr)) \
+	{ \
+		DBG_LOGF("ASSERT FAILED!\n\tExpression: %s\n\tFile: %s, Line: %d\n", #Expr, __FILE__, __LINE__); \
+	}
+#define THE_PIT_UNUSED(Var) (void)Var
+
+#endif // THE_PIT_H
