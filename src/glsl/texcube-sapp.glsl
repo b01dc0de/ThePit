87948148b2dae03f99069db5eeda6b852aab1cc4
CKA <163610791+b01dc0de@users.noreply.github.com>
2024-10-24 17:31:05 -0500

    Remove commented out color vertex attrib from sokol-samples/texcube

--- src/glsl/texcube-sapp.glsl
+++ src/glsl/texcube-sapp.glsl
@@ -12,15 +12,12 @@ uniform vs_params {
 };
 
 in vec4 pos;
-//in vec4 color0;
 in vec2 texcoord0;
 
-//out vec4 color;
 out vec2 uv;
 
 void main() {
     gl_Position = mvp * pos;
-//    color = color0;
     uv = texcoord0 * 5.0;
 }
 #pragma sokol @end
@@ -29,12 +26,10 @@ void main() {
 uniform texture2D tex;
 uniform sampler smp;
 
-//in vec4 color;
 in vec2 uv;
 out vec4 frag_color;
 
 void main() {
-//    frag_color = texture(sampler2D(tex,smp), uv) * color;
     frag_color = texture(sampler2D(tex,smp), uv);
 }
 #pragma sokol @end
