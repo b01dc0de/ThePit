9f38e2e8c4f6ac92cbd68738aaaf40bcfc14d1bc
CKA <cka.txt@gmail.com>
2024-10-25 13:39:26 -0500

    Update build-linux.sh with all translation units in src/

--- build-linux.sh
+++ build-linux.sh
@@ -4,7 +4,7 @@
 #		- ../sokol-tools-bin has been built in the parent directory
 #	Would like to change this in the future but we'll burn that bridge when we get there
 
-SRC_FILES="src/Main.cpp"
+SRC_FILES="src/Main.cpp src/Cube.cpp src/DrawState.cpp src/Engine.cpp src/Geometry.cpp src/Material.cpp src/Mesh.cpp"
 OUT_DIR=out/build/linux
 OUT_FILE=thepit
 LIB_DIR=./libs/sokol
@@ -13,11 +13,10 @@ BUILD_FLAGS="-g -pthread"
 
 mkdir -p $OUT_DIR
 
-SOKOL_SHDC=../sokol-tools-bin/bin/linux/sokol-shdc
-GLSL_DIR=src/glsl
-SHADER_FILE=$GLSL_DIR/vxcolor.glsl
-
 # Use Sokol shader compiler:
-$SOKOL_SHDC -i $SHADER_FILE -o $SHADER_FILE.h -l glsl410
+#SOKOL_SHDC=../sokol-tools-bin/bin/linux/sokol-shdc
+#GLSL_DIR=src/glsl
+#SHADER_FILE=$GLSL_DIR/vxcolor.glsl
+#$SOKOL_SHDC -i $SHADER_FILE -o $SHADER_FILE.h -l glsl410
 
 g++ $BUILD_FLAGS -o $OUT_DIR/$OUT_FILE -I $LIB_DIR $SRC_FILES $LINK_LIBS
