5aa6e6cf2e6981504a8a58b3a185b13ff07c6efe
CKA <163610791+b01dc0de@users.noreply.github.com>
2024-10-28 19:01:40 -0500

    Space-ify Engine.h (whoops)

--- src/Engine.h
+++ src/Engine.h
@@ -9,30 +9,30 @@
 
 namespace thepit
 {
-	struct GlobalState_t
-	{
-		Material* cube_material = nullptr;
-		Mesh* cube_mesh = nullptr;
+    struct GlobalState_t
+    {
+        Material* cube_material = nullptr;
+        Mesh* cube_mesh = nullptr;
 
-		MeshDrawT* texcube_geometry = nullptr;
-		DrawStateT* tex_drawstate = nullptr;
-		MeshDrawT* colcube_geometry = nullptr;
-		DrawStateT* col_drawstate = nullptr;
+        MeshDrawT* texcube_geometry = nullptr;
+        DrawStateT* tex_drawstate = nullptr;
+        MeshDrawT* colcube_geometry = nullptr;
+        DrawStateT* col_drawstate = nullptr;
 
-		GeometryT* singlecolorcube = nullptr;
-		GeometryT* floormesh = nullptr;
+        GeometryT* singlecolorcube = nullptr;
+        GeometryT* floormesh = nullptr;
 
-		FPSView shooter;
-	};
-	extern GlobalState_t GlobalState;
+        FPSView shooter;
+    };
+    extern GlobalState_t GlobalState;
 
-	extern int global_argc;
-	extern char** global_argv;
+    extern int global_argc;
+    extern char** global_argv;
 
-	void Init();
-	void Frame();
-	void Cleanup();
-	void HandleEvent(const sapp_event* Event);
+    void Init();
+    void Frame();
+    void Cleanup();
+    void HandleEvent(const sapp_event* Event);
 
     const int WindowWidth = 640;
     const int WindowHeight = 480;
