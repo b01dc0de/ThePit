01908cab028d2619e34e110ffc8e1cd59a2f07aa
Your Name <you@example.com>
2024-10-29 21:02:10 -0500

    Updated mac main

--- src/MacOS_Main.mm
+++ src/MacOS_Main.mm
@@ -1,3 +1,26 @@
-#if THEPIT_PLATFORM_MACOS()
-#include "Main.cpp"
-#endif // THEPIT_PLATFORM_MACOS()
+#define SOKOL_IMPL
+//#define SOKOL_APP_IMPL
+// TODO: What's the difference between SOKOL_IMPL and SOKOL_APP_IMPL?
+#include "Common.h"
+#include "Engine.h"
+
+extern sapp_desc sokol_main(int argc, char* argv[])
+{
+	thepit::HandleCmdLine(argc, argv);
+
+	sapp_desc thepit_desc = {};
+	{
+		thepit_desc.init_cb = thepit::Init;
+		thepit_desc.frame_cb = thepit::Frame;
+		thepit_desc.cleanup_cb = thepit::Cleanup;
+		thepit_desc.event_cb = thepit::HandleEvent;
+		thepit_desc.width = thepit::WindowWidth;
+		thepit_desc.height = thepit::WindowHeight;
+		thepit_desc.sample_count = 1;
+		thepit_desc.high_dpi = true;
+		thepit_desc.fullscreen = false;
+		thepit_desc.window_title = "DO NOT FALL IN THE PIT";
+		thepit_desc.logger.func = slog_func;
+	}
+	return thepit_desc;
+}
